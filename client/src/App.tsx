67e2c056e6beb50e235fc4b9d1ce1221e03f2b42
jina <wlsdk7245@gmail.com>
2023-06-13 10:39:29 +0900

    chore: zustand, 메인 페이지 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import MainPage from 'pages/MainPage';
 
 const App = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
 };
 
 export default App;
