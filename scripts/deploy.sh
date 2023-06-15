5d8b74b6bca2af42cd761fdb00e776a8908645ae
lsh4711 <dowkwll@naver.com>
2023-06-16 05:23:02 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,4 +21,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/team-014/server/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ec2-user/action/team-014/server/deploy.log 2>/home/ec2-user/action/team-014/server/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/action/team-014/server/deploy.log 2>/home/ec2-user/action/team-014/server/deploy_err.log &
