85da18c5c17918cdf60d8196efff79dfc95ff393
jina <wlsdk7245@gmail.com>
2023-06-15 14:55:58 +0900

    feat: 메인페이지 제목 추가

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,19 +1,55 @@
 import styled from 'styled-components';
-import QuestionCard from 'feature/Main/QuestionCard';
+import { useNavigate } from 'react-router-dom';
+import Button from 'components/Button/Button';
+import QuestionList from 'feature/Main/QuestionList';
 import { questionList } from 'feature/Main/mockData';
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
+  const onClickButton = () => navigate('/ask');
+
   return (
     <StyledMainPage>
-      {questionList.map((question, idx) => (
-        <QuestionCard key={`question-card-${idx}`} questionData={question} />
-      ))}
+      <MainTop>
+        <Title>
+          <TitleText>All Questions</TitleText>
+          <Button onClick={onClickButton}>Ask Question</Button>
+        </Title>
+        <QuestionCount>
+          {questionList.length.toLocaleString()} questions
+        </QuestionCount>
+      </MainTop>
+      <QuestionList questionList={questionList} />
     </StyledMainPage>
   );
 };
 
 const StyledMainPage = styled.div`
   max-width: 728px;
+`;
+
+const MainTop = styled.div`
+  padding: 24px 16px 0;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-bottom: 12px;
+`;
+
+const TitleText = styled.div`
+  font-size: 22px;
+  line-height: 1.3;
+  margin: 0 12px 12px 0;
+`;
+
+const QuestionCount = styled.div`
+  font-size: 16px;
+  margin-bottom: 12px;
 `;
 
 export default MainPage;
