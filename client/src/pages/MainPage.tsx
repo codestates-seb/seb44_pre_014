ca3508c90449795e8458800c4d5afa1d0988fb85
jina <wlsdk7245@gmail.com>
2023-06-15 13:58:38 +0900

    feat: 메인페이지에 카드리스트 추가

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components';
+import QuestionCard from 'feature/Main/QuestionCard';
+import { questionList } from 'feature/Main/mockData';
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <StyledMainPage>
+      {questionList.map((question, idx) => (
+        <QuestionCard key={`question-card-${idx}`} questionData={question} />
+      ))}
+    </StyledMainPage>
+  );
 };
+
+const StyledMainPage = styled.div`
+  max-width: 728px;
+`;
 
 export default MainPage;
