93ba5ecbc1b38faba57b8855385d58c55f98923c
lsh4711 <dowkwll@naver.com>
2023-06-21 11:16:51 +0900

    Remove
    remove name field

--- server/src/main/java/com/server/member/dto/MemberDto.java
+++ server/src/main/java/com/server/member/dto/MemberDto.java
@@ -20,9 +20,6 @@ public class MemberDto {
         @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,20}$", message = "password 길이는 최소 8자 이상 최대 20자 이하, 숫자 1자 이상, 대소문자 구분없이 영문자 1자 이상, 특수문자 1자 이상 입력 해주세요.")
         private String password;
 
-        @NotBlank(message = "성함을 작성해 주세요.")
-        private String name;
-
         @NotBlank(message = "닉네임을 작성해 주세요")
         private String username;
     }
