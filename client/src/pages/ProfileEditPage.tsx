bf2831d5c2d0d1fe63af19771584738c251f58b1
jina <wlsdk7245@gmail.com>
2023-06-23 18:59:10 +0900

    fix: 프로필 수정 페이지는 본인만 접근 가능하게 수정

--- client/src/pages/ProfileEditPage.tsx
+++ client/src/pages/ProfileEditPage.tsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 import ProfileEdit from 'feature/ProfileEdit/ProfileEdit';
+import { useGetUserInfo } from 'store/userInfo/store.userInfo';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const ProfileEditPage = () => {
+  const userInfo = useGetUserInfo();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  if (userInfo.memberId !== Number(id)) navigate(`/profile/${id}`);
   return (
     <StyledProfileEditPage>
       <ContentContainer>
