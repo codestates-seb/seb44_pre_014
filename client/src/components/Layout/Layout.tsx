f65c1d975123711114f1d2121bd974c0bd5e82db
jina <wlsdk7245@gmail.com>
2023-06-26 14:48:20 +0900

    feat: store수정, 전체 레이아웃 너비값 조정

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -32,6 +32,7 @@ const DivContainer = styled.div`
 const ContentContainer = styled.div`
   margin: 3.5em 0px 0px 0px;
   max-width: 1100px;
+  width: 100%;
   min-height: calc(100vh - 56px);
 
   @media (max-width: 768px) {
--- client/src/store/user/store.user.ts
+++ client/src/store/user/store.user.ts
@@ -2,10 +2,12 @@ import create from 'zustand';
 
 interface StoreState {
   memberId: string | null;
+  isLoading: boolean;
   setMemberId: (memberId: string | null) => void;
 }
 
 export const useUserStore = create<StoreState>((set) => ({
   memberId: null,
-  setMemberId: (memberId) => set({ memberId }),
+  isLoading: true,
+  setMemberId: (memberId) => set({ isLoading: false, memberId }),
 }));
