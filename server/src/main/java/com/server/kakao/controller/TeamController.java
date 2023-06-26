81a89bf5b8f02abf12c818563a5e7d63e461bb9d
lsh4711 <dowkwll@naver.com>
2023-06-27 00:32:12 +0900

    백엔드팀 중간 결과물

--- server/src/main/java/com/server/kakao/controller/TeamController.java
+++ server/src/main/java/com/server/kakao/controller/TeamController.java
@@ -15,6 +15,6 @@ public class TeamController {
     public String getTeamPage(HttpServletResponse response) throws IOException {
         String redirecUrl = "https://marred-wavelength-6bf.notion.site/caafba1487854c3ca7b2613937442662?v=931dc3d47e0248d4a9c6940502dbf61d";
         response.sendRedirect(redirecUrl);
-        return "동료가 되어주세요~~";
+        return "동료가 되어주세요";
     }
 }
