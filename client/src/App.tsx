3bf1228f0effc02c2c0ffa903c2522777f593c5a
chen4023 <106866926+chen4023@users.noreply.github.com>
2023-06-26 10:15:58 +0900

    [FE] 질문 상세페이지 구현 (#52)
    * feat:질문 상세 페이지 틀 구현
    
    * feat: votecount기능 추가
    
    * feat: VoteCount기능 수정
    
    * feat:질문&답변데이터 받아오기
    
    * feat: 질문 삭제,수정 기능구현
    
    * feat:댓글 추가 기능 구현
    
    * feat: 답변삭제, 질문 이미지파일 불러오기 기능 구현
    
    * feat: 날짜데이터 문자열로 변경하는 함수 구현
    
    * 게시글 수정 api주소 변경
    
    * feat: 답변수정 기능구현
    
    ---------
    
    Co-authored-by: Kim Jina <71418418+wlsdk7245@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from 'pages/MainPage';
+import DetailQuestion from 'pages/DetailQuestion';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/questions/:id" element={<DetailQuestion />} />
     </Routes>
   );
 };
---
+++ client/src/feature/QuestionDetail/DetailAnswer.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import VoteContainer from './VoteContainer';
+import LabelContainer from './LabellContainer';
+import { TQuestion } from 'utils/type';
+import EditAnswer from './EditAnswer';
+
+type Tprops = {
+  quData: TQuestion;
+  deleteQu: (id: number, type: string) => void;
+  updateQu: (id: number, type: string) => void;
+  isEdit: boolean;
+  setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DetailAnswer: React.FC<Tprops> = ({
+  quData,
+  deleteQu,
+  updateQu,
+  isEdit,
+  setIsEdit,
+}) => {
+  return (
+    <>
+      <h3>{quData.answers.length} Answer</h3>
+      {quData.answers.map(({ answerId, content }) => (
+        <AnswerContainer>
+          <VoteContainer />
+          <AnswerMain>
+            {isEdit && (
+              <EditAnswer
+                setIsEdit={setIsEdit}
+                content={content}
+                id={answerId}
+              />
+            )}
+            {!isEdit && (
+              <AnswerText>
+                <p key={answerId}>{content}</p>
+              </AnswerText>
+            )}
+            <LabelContainer
+              quData={quData}
+              deleteQu={deleteQu}
+              id={answerId}
+              updateQu={updateQu}
+              type="answer"
+            />
+          </AnswerMain>
+        </AnswerContainer>
+      ))}
+    </>
+  );
+};
+
+export default DetailAnswer;
+
+const AnswerContainer = styled.section`
+  display: flex;
+`;
+
+const AnswerText = styled.div`
+  font-size: 16px;
+  padding: 15px;
+`;
+
+const AnswerMain = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
