07ad9f5ab4dc6373bef2a3c2656a614a47ab463c
lsh4711 <dowkwll@naver.com>
2023-06-27 03:00:34 +0900

    test

--- server/src/main/java/com/server/kakao/controller/TeamController.java
+++ server/src/main/java/com/server/kakao/controller/TeamController.java
@@ -14,7 +14,9 @@ public class TeamController {
     @GetMapping
     public String getTeamPage(HttpServletResponse response) throws IOException {
         String redirecUrl = "https://marred-wavelength-6bf.notion.site/caafba1487854c3ca7b2613937442662?v=931dc3d47e0248d4a9c6940502dbf61d";
+
         response.sendRedirect(redirecUrl);
+
         return "동료가 되어주세요";
     }
 }
